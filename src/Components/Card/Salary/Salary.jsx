5044edfd275b4eb1bea466915990f9f65577d5f6
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-13 22:09:17 +0600

    Player data in json format added

---
+++ src/Components/Card/Salary/Salary.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Salary = () => {
+    return (
+        <div>
+            <p>Salary: $6000</p>
+        </div>
+    );
+};
+
+Salary.propTypes = {
+    
+};
+
+export default Salary;
--- src/Components/Card/Title.jsx
+++ src/Components/Card/Title.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import Salary from './Salary/Salary';
 
 const Title = () => {
     return (
         <div>
             <div className="card-body items-center text-center">
     <h2 className="card-title">Shape of You</h2>
+    <Salary />
     <Button />
   </div>
         </div>
